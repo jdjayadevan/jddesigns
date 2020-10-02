deaef4d4c1720465b64e1a292e1f28182516ea6f
raj1103 <raj.civil1103@gmail.com>
2020-10-02 18:32:41 +0530

    021020201745

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,51 +1,72 @@
-$('#send').click(function() {
-    var name = $("input#name").val();
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
           
-    var email = $("input#email").val();
-    var message = $("input#message").val();
-    var phone = $("input#phone").val();
-    var company = $("textarea#company").val();
-    var firstName = name; // For Success/Failure Message
-    
-    // Check for white space in name for Success/Fail message
-    if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-    }
-    $.ajax({
-        url: "https://jddesign.000webhostapp.com/mail/contact_me1.php",
-        type: "POST",
-        data: {
-            name: name,
-            email: email,
-            message: message,
-            phone: phone,
-            company: company,
-        },
-       
-        cache: false,
+            var email = $("input#email").val();
+            var message = $("input#message").val();
+            var phone = $("input#phone").val();
+            var company = $("textarea#company").val();
+            var firstName = name; // For Success/Failure Message
+            alert(name)
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://jddesign.000webhostapp.com/mail/contact_me1.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message,
+                    phone: phone,
+                    company: company,
+                },
 
-        success: function() {
-            // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                .append("</button>");
-            $('#success > .alert-success')
-                .append("<strong>Your message has been sent. </strong>");
-            $('#success > .alert-success')
-                .append('</div>');
+                cache: false,
 
-            //clear all fields
-           alert("sucess")
-        },
-        error: function() {
-            // Fail message
-          
-            alert("fail")
-        },
-    })
-    alert(data)
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
 
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                  
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
 });
+
+
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
